7a17c3e19bf90818a2bac7eb17d80f735a911ca3
hanna <hanna.aspblad@iths.se>
2021-11-04 12:17:46 +0100

    added tie results

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './flags32-both.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./flags32-both.css";
+import App from "./App";
+
+localStorage.setItem("improvedScoring", false);
+localStorage.setItem("improvedFlagging", false);
+localStorage.setItem("improvedResult", false);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
