9d3d46e5970591e3ca85a14ce3aecbc9e51a78af
hanna <hanna.aspblad@iths.se>
2021-11-05 12:06:37 +0100

    added test for duplicate correct answers

--- app/src/tests/test_index.js
+++ app/src/tests/test_index.js
@@ -2,11 +2,22 @@ import assert from "assert";
 import { createGame } from "../utils.js";
 
 describe("checkDuplicateAlternatives", () => {
-    it("should return 4", () => {
-        const result = createGame("improvedQuestions");
-        const questionsArr = Object.values(result.questions);
-        const alt = Object.values(questionsArr[0].alternatives);
-        const checkDuplicate = new Set(alt).size;
-        assert.equal(alt.length, checkDuplicate);
-    });
+  it("should return 4", () => {
+    const result = createGame("improvedQuestions");
+    const questionsArr = Object.values(result.questions);
+    const alt = Object.values(questionsArr[0].alternatives);
+    const checkDuplicate = new Set(alt).size;
+    assert.equal(alt.length, checkDuplicate);
+  });
+});
+
+describe("checkAnswers", () => {
+  it("should return 5", () => {
+    let answersArr = [];
+    const result = createGame("improvedQuestions");
+    const questionsArr = Object.values(result.questions);
+    questionsArr.forEach((r) => answersArr.push(r.correct));
+    const checkDuplicate = new Set(answersArr).size;
+    assert.equal(answersArr.length, checkDuplicate);
+  });
 });
